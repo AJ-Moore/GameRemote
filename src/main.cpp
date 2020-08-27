ca62f7b7d07b89110b304cbd24d397f9167a3620
Alzon <allan.j.moore@outlook.com>
2020-08-27 21:48:54 +0100

    Update cmake

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,10 @@
 #define PSVITA
 
 #ifdef PSVITA
+#include <psp2/kernel/threadmgr.h>
 #include <psp2/kernel/processmgr.h>
+
+#define printf psvDebugScreenPrintf
 #endif
 
 #include "Engine.h"
@@ -18,6 +21,7 @@ int main(int argc, char *argv[])
 	engine.Start();
 
 #ifdef PSVITA
+	sceKernelDelayThread(10 * 1000000); // Wait for 3 seconds
 	sceKernelExitProcess(0);
 #endif
     return 0;
